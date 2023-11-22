3c788d5fd22cbc8b1ad7d03032c37165bca9252d
LFQIIIH <belaouadyounes13@gmail.com>
2023-11-22 19:16:32 +0100

    Betty style

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 
 /**
- * create_array -  creates an array of chars, and initializes it with a specific char.
- * @size : size of the array.
- * @c : the character.
+* create_array -  creates an array of chars, and initializes it with a specific char.
+* @size : size of the array.
+* @c : the character.
 */
 
 char	*create_array(unsigned int size, char c)
@@ -13,7 +13,6 @@ char	*create_array(unsigned int size, char c)
 	char	*buffer;
 	unsigned int	i;
 
-	
 	buffer = (char *)malloc(sizeof(char) * size);
 	if (buffer == NULL || !size)
 		return (NULL);
@@ -23,6 +22,5 @@ char	*create_array(unsigned int size, char c)
 		buffer[i] = c;
 		i++;
 	}
-
 	return (buffer);
-} 
+}
