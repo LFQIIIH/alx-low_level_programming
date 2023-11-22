3f02a67144f91870fb6804edd793b843f12f17ed
LFQIIIH <belaouadyounes13@gmail.com>
2023-11-22 19:34:41 +0100

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -34,7 +34,7 @@ char	*str_concat(char *s1, char *s2)
 	char			*new_str;
 
 	if (s1 == NULL && s2 == NULL)
-		return (NULL);
+		return ("");
 	lenght = _strlen(s1) + _strlen(s2) + 1;
 	new_str = (char *)malloc(sizeof(char) * lenght);
 	if (new_str == NULL)
