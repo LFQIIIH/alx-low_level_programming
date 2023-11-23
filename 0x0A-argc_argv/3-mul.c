310e9d260baadc15e26cf8fbd4381444d02a7468
MAR BEN <marouanebenbajja@gmail.com>
2023-11-23 03:15:15 +0100

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* main - prints the name of the program
+*
+* Return : Always 0.
+*/
+
+int main(int argc, char **argv)
+{
+	int	x;
+	int	y;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	printf("%d\n", x*y);
+	return (0);
+}
