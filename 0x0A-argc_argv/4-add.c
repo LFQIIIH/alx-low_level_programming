a00f6be580e9b39295fc6cd82e2af6e4b6bc4b14
MAR BEN <marouanebenbajja@gmail.com>
2023-11-23 03:27:49 +0100

    4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* main - check the code for ALX School students.
+*
+* Return: Always 0.
+*/
+
+int main(int argc, char **argv)
+{
+	int	res;
+	int	i;
+	int	n;
+
+	i = 1;
+	res = 0;
+	while (i < argc)
+	{
+		if (!(argv[i][0] >= 48 && argv[i][0] <= 57))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		n = atoi(argv[i]);
+		res = res + n;
+		i++;
+	}
+	printf("%d\n", res);
+	return (0);
+}
