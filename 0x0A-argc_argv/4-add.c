af5c0af4eaf7c7b49144033f9d0d5e74f559a137
MAR BEN <marouanebenbajja@gmail.com>
2023-11-23 03:44:48 +0100

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * check - check the args validation. 
+ * 
+ * @arg: the argument to check. 
+ *
+ * Return: 0 if valid arg 1 if not.
+ */
+
+int check(char *arg)
+{
+	int	i;
+
+	i = 0;
+	while (arg[i])
+	{
+		if (!(arg[i] >= 48 && arg[i] <= 57))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+/**
 * main - Prints the number of arguments passed into main
 * @argc: Number of command line arguments
 * @argv: Array name
@@ -17,7 +39,7 @@ int main(int argc, char **argv)
 	res = 0;
 	while (i < argc)
 	{
-		if (!(argv[i][0] >= 48 && argv[i][0] <= 57))
+		if (check(argv[i]))
 		{
 			printf("Error\n");
 			return (1);
