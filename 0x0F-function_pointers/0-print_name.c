66480f2a8d598c35447d13735cfe0adcbb4b9c73
LFQIIIH <belaoouadyounes13@gmail.com>
2023-11-30 07:36:46 -0800

    Betty

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints the name using the function pointed by second argument. 
- * 
- * @name: name of person. 
+ * print_name - prints the name using the function pointed by second argument.
+ *
+ * @name: name of person.
  * @f: Pointer to used function
  *
- * Return: No return. 
+ * Return: No return.
  */
 void	print_name(char *name, void (*f)(char *))
 {
