792d4b3fd8d6d8057a4f86f63d82a5ab3ab3693d
LFQIIIH <belaoouadyounes13@gmail.com>
2023-11-30 04:39:46 -0800

    0-print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_name - prints the name using the function pointed by second argument. 
+ * 
+ * @name: name of person. 
+ * @f: Pointer to used function 
+ */
+void	print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/main.h
@@ -0,0 +1,7 @@
+#ifndef	_MAIN_H
+#define	_MAIN_H
+
+int		_putchar(char c);
+void	print_name(char *name, void (*f)(char *));
+
+#endif
